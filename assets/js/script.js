a543e4b143eca161117f6e3d1368bfbe54b50061
Bulochka-s-klubnichkoi <sofia.biletskaya15092010@gmail.com>
2021-09-17 19:46:24 +0300

    Хреновый но слайдер

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,19 @@ function searchButton() {
    input.style.cursor = "text";
 }
 button.addEventListener("click", searchButton);
+
+var arrowL = document.getElementsByClassName("main-section3-slider-arrows-left")[0];
+var arrowR = document.getElementsByClassName("main-section3-slider-arrows-right")[0];
+
+var currentpic = document.querySelector(".main-section3-slider-img");
+var currentpicid = 0;
+
+
+
+arrowL.addEventListener("click", previous);
+
+function previous() {
+   console.log("ауууууу а ну ладно")
+   // currentpicid = currentpicid + 1;
+   currentpic.scr = "http://127.0.0.1:5500/assets/img/section3/slider1.jpg";
+}
